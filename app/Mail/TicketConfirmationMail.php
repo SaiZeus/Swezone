ec249dafcf8e8a083043f47ccb9456e392ce19a2
SaiZeus <zeuspower200@gmail.com>
2026-09-11 09:08:06 +0630

    hide

--- app/Mail/TicketConfirmationMail.php
+++ app/Mail/TicketConfirmationMail.php
@@ -36,7 +36,7 @@ class TicketConfirmationMail extends Mailable implements ShouldQueue // Added im
         $formattedTicketRef = 'BGR' . str_pad($attendee->id, 5, '0', STR_PAD_LEFT);
 
         // Load background ticket image for PDF and email view
-        $bgPath = public_path('assets/img/img/ticket/ticket.jpg');
+        $bgPath = public_path('assets/img/ticket/ticket.jpg');
         $ticketBgBase64 = file_exists($bgPath) ? 'data:image/jpeg;base64,' . base64_encode(file_get_contents($bgPath)) : null;
 
         // Generate local QR code matching the ticket reference instead of external API
